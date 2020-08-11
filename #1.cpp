d329f3532c6c3b1fa644bcc0f95a4084d5869ca3
Khushi Mishra <66117883+khushimishra26@users.noreply.github.com>
2020-08-11 10:33:40 +0530

    Create #1.cpp

---
+++ #1.cpp
@@ -0,0 +1,16 @@
+//To find size of all fundamental data types in C++.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<<"Size of fundamental data types is:"<<endl;
+cout<<"char size"<<sizeof(char)<<endl;
+cout<<"short size"<<sizeof(short)<<endl;
+cout<<"long size"<<sizeof(long)<<endl;
+cout<<"int size"<<sizeof(int)<<endl;
+cout<<"float size"<<sizeof(float)<<endl;
+cout<<"double size"<<sizeof(double)<<endl;
+cout<<"long double size"<<sizeof(long double)<<endl;
+cout<<"boolean size"<<sizeof(bool)<<endl;
+return 0;}
